15a6e73617614a452d25b47826dc540b3ab21fec
Dian Buana Putra <dianbuana2069@gmail.com>
2021-06-28 20:52:36 +0700

    login

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,76 +1,88 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-                @if (session('error'))
-                    <div class="alert alert-danger">{{ session('error') }}</div>
-                @endif
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="NIK/Email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="ID" type="text" class="form-control @error('email') is-invalid @enderror" name="ID" value="{{ old('email') }}" required autocomplete="ID" autofocus>
-
-                                @error('ID')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<html>
+  <head>
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous">
+    </script>
+    <link rel="icon" type="image/png" href="logo_jember.png">
+    <link rel="stylesheet" href="style.css" />
+    <title>Sistem Informasi Pelayanan Adminduk dan Pencapil | Dinas Kependudukan dan Pencatatan Sipil Kabupaten Jember</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="#" class="sign-in-form">
+            <h2 class="title">Masuk</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="NIK atau Alamat Email" />
             </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Kata Sandi" />
+            </div>
+            <input type="submit" value="Masuk" class="btn solid" />
+          </form>
+
+          <form action="#" class="sign-up-form">
+            <h2 class="title">Daftar Baru</h2>
+
+            <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="number" placeholder="NIK" />
+            </div>
+
+            <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="email" placeholder="Alamat Email" />
+            </div>
+
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Kata Sandi" />
+            </div>
+
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Konfirmasi Kata Sandi" />
+            </div>
+            
+            <input type="submit" class="btn" value="Daftar Baru" />
+          </form>
         </div>
+      </div>
+
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>SELAMAT DATANG</h3>
+            <p>
+              Selamat Datang di Website Resmi Disdukcapil
+              Kabupaten Jember
+            </p>
+            <button class="btn transparent" id="sign-up-btn">
+              Daftar Baru
+            </button>
+          </div>
+          <img src="img/log.svg" class="image" alt="" />
+        </div>
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>SELAMAT DATANG</h3>
+            <p>
+              Selamat Datang di Website Resmi Disdukcapil
+              Kabupaten Jember
+            </p>
+            <button class="btn transparent" id="sign-in-btn">
+              Masuk
+            </button>
+          </div>
+          <img src="img/register.svg" class="image" alt="" />
+        </div>
+      </div>
     </div>
-</div>
-@endsection
+
+    <script src="app.js"></script>
+  </body>
+</html>
