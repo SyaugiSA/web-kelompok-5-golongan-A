35857a99f1e77832b918b86e91d4a7b36cebe2da
devi fwi indra lestari <devidwiindralestari@gmail.com>
2021-07-06 12:33:27 +0700

    form ktp
    form ktp

---
+++ resources/views/frontend/ktp_user/detail.blade.php
@@ -0,0 +1,93 @@
+@extends('frontend/layouts.template')
+@section('content')
+      <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">FORM PENGAJUAN CETAK KTP</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- column -->
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body"> 
+                                <form action="/ktp_user/store" method="POST" enctype="multipart/form-data">
+                                    {{ csrf_field() }}
+                                    <div class="mb-3">
+                                      <label class="form-label">Nomor NIK</label>
+                                      <input type="text" class="form-control" value="{{$aktakelahiran->NIK}}" placeholder="{{$aktakelahiran->NIK}}" id="NIK_Pengajuan" name="NIK">
+                                    </div>
+                                    <div class="mb-3">
+                                      <label class="form-label">Nama Lengkap</label>
+                                      <input type="text" class="form-control" value="{{$aktakelahiran->nama_lengkap}}" placeholder="{{$aktakelahiran->nama_lengkap}}" name="Nama_Lengkap">
+                                    </div>
+                                    <div class="mb-3">
+                                      <label class="form-label">Foto diri</label>
+                                      <input type="file" name="foto_diri" class="form-control" >
+                                    </div>
+                                    <div class="mb-3">
+                                      <label class="form-label">Nomor Kartu-Keluarga</label>
+                                      <input placeholder="{{$aktakelahiran->no_KK}}" value="{{$aktakelahiran->no_KK}}" type="text" name="no_KK" class="form-control" >
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label">Surat Keterangan</label>
+                                        <input type="file" name="surat_keterangan" class="form-control" >
+                                    </div>
+                                    <div class="mb-3">
+                                      <label class="form-label">Tanggal Pengajuan</label>
+                                      <input type="text" name="tanggal" id="tanggal_pembuatan" class="form-control tanggal_pembuatan" >
+                                    </div>
+                                   
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                  </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+           
+@endsection
+
+@push('content-css')
+    {{-- <link rel="stylesheet" href="js/jquery-ui/jquery-ui.css"> --}}
+    <link rel="stylesheet" href="{{asset("frontend/jquery/jquery-ui.css")}}">
+@endpush
+
+@push('content-js')
+    <script src="{{asset("frontend/jquery/jquery.js")}}"></script>
+    <script src="{{asset("frontend/jquery/jquery-ui.js")}}"></script>
+  
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#tanggal_pembuatan').datepicker({
+                dateFormat:"yy-mm-dd",
+            });		
+        });
+    </script>
+@endpush
---
+++ resources/views/frontend/ktp_user/index.php
@@ -0,0 +1,78 @@
+@extends('frontend/layouts.template')
+@section('content')
+      <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Data Pengajuan Cetak KTP Status PRR</h4>
+                                <h6 class="card-subtitle">KHUSUS BAGI WARGA YANG PERNAH MELAKUKAN PEREKAMAN DATA DIRI </h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- column -->
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body">
+                               <a href="/ktp_user/create/{{$user}}" class="btn btn-primary">Pengajuan Baru</a>
+                                <div class="table-responsive">
+                                    <table class="table user-table">
+                                        <thead>
+                                            <tr>
+                                                <th class="border-top-0">No</th>
+                                                <th class="border-top-0">NIK PEMOHON</th>
+                                                <th class="border-top-0">NIK TERDAFTAR</th>
+                                                <th class="border-top-0">NAMA LENGKAP</th>
+                                                <th class="border-top-0">TANGGAL</th>
+                                                <th class="border-top-0">STATUS</th>
+                                                <th class="border-top-0">AKSI</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @php
+                                                $no = 1;
+                                            @endphp
+                                             @foreach ($data as $item)
+                                                 <tr>
+                                                     <td>{{$no++}}</td>
+                                                     <td>{{$item->NIK}}</td>
+                                                     <td>{{$item->NIK}}</td>
+                                                     <td>{{$item->Nama_Lengkap}}</td>
+                                                     <td>{{$item->tanggal}}</td>
+                                                     <td><button disabled class="btn btn-warning">{{$item->status}}</button></td>
+                                                 </tr>
+                                             @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+@endsection
