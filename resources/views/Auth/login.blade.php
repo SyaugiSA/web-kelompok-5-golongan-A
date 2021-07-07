83167f1e15bcca33da43574fd15d8d2bedeb06b7
Syaugi Salim Amar <71821834+SSA5461@users.noreply.github.com>
2021-07-07 12:47:59 +0700

    Update login.blade.php

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -4,48 +4,63 @@
       src="https://kit.fontawesome.com/64d58efce2.js"
       crossorigin="anonymous">
     </script>
-    <link rel="icon" type="image/png" href="logo_jember.png">
-    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" type="image/png" href="{{asset('Auth/login/logo_jember.png')}}">
+    <link rel="stylesheet" href="{{asset('Auth/login/style.css')}}" />
     <title>Sistem Informasi Pelayanan Adminduk dan Pencapil | Dinas Kependudukan dan Pencatatan Sipil Kabupaten Jember</title>
   </head>
   <body>
     <div class="container">
       <div class="forms-container">
         <div class="signin-signup">
-          <form action="#" class="sign-in-form">
+          <form method="POST" action="{{ route('login') }}" class="sign-in-form">
+            @csrf
             <h2 class="title">Masuk</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
-              <input type="text" placeholder="NIK atau Alamat Email" />
+              <input id="ID" type="text" class="form-control @error('email') is-invalid @enderror" name="ID" value="{{ old('email') }}" required autocomplete="ID" autofocus placeholder="NIK atau Alamat Email" />
             </div>
             <div class="input-field">
               <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Kata Sandi" />
+              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Kata Sandi" />
             </div>
             <input type="submit" value="Masuk" class="btn solid" />
           </form>
 
-          <form action="#" class="sign-up-form">
+          <form action="{{ route('register') }}" method="POST" class="sign-up-form">
+            @csrf
             <h2 class="title">Daftar Baru</h2>
 
             <div class="input-field">
               <i class="fas fa-envelope"></i>
-              <input type="number" placeholder="NIK" />
+              <input id="NIK" class="form-control @error('NIK') is-invalid @enderror" name="NIK" value="{{ old('NIK') }}" required autocomplete="NIK" autofocus type="number" placeholder="NIK" />
             </div>
 
             <div class="input-field">
               <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="Alamat Email" />
+              <input id="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" type="email" placeholder="Alamat Email" />
             </div>
+
+              @error('email')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+
 
             <div class="input-field">
               <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Kata Sandi" />
+              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Kata Sandi" />
             </div>
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
             <div class="input-field">
               <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Konfirmasi Kata Sandi" />
+              <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Konfirmasi Kata Sandi" />
             </div>
             
             <input type="submit" class="btn" value="Daftar Baru" />
@@ -83,6 +98,6 @@
       </div>
     </div>
 
-    <script src="app.js"></script>
+    <script src="{{asset('Auth/login/app.js')}}"></script>
   </body>
 </html>
