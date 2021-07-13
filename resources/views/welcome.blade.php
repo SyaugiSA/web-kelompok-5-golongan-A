c42021dad3d4e205d2032e676de4c822be369e8e
Dian Buana Putra <dianbuana2069@gmail.com>
2021-07-13 10:23:39 +0700

    revisi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,8 +48,8 @@
 	</div>
 	<nav class="navbar navbar-expand-lg navigation" id="navbar">
 		<div class="container">
-		 	 <a class="navbar-brand" href="index.html">
-			  	<img src="images/logo_small.png" alt="" class="img-fluid">
+		 	 <a class="navbar-brand" href="welcome.blade.php">
+			  	<img src="{{asset('Auth/login/logo_small.png')}}" alt="" class="img-fluid">
 			  </a>
 
 		  	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
